d3795adfbce76398b09d4fdb22410b815718b935
Matas Seimys <mseimys@vtex.lt>
2015-10-06 16:12:45 +0300

    Some modifications

--- public/application.js
+++ public/application.js
@@ -2,33 +2,93 @@
 
     'use strict';
 
+    function Company(name){
+        this.name = name;
+        this.element = $('<div>' + name + '</div>');
+    }
+
+    function CompanyList(dest){
+        var companies = [];
+        var destination = $(dest);
+
+        this.addCompany = function(name) {
+            companies.push(new Company(name));
+        }
+        this.generateList = function() {
+            destination.empty();
+            for (var i=0; i<companies.length; i++) {
+                destination.append(companies[i].element);
+            }
+        }
+        this.clear = function(){
+            destination.empty();
+            companies = [];
+        }
+    };
+
+    // var cl = new CompanyList('#results');
+    // console.log(cl);
+    // cl.addCompany('company1');
+    // cl.addCompany('company2');
+    // cl.generateList();
+
+    function initializeUI(){
+        var $loading = $('#spinner').hide();
+        $(document)
+            .ajaxStart(function () {
+                $loading.show();
+            })
+            .ajaxStop(function () {
+                $loading.hide();
+            });
+    }
+
     $(document).ready(function () {
-        var input = $('.search-input');
-        var button = $('.search-button');
-        var result = $('.result');
+        function showResult(data) {
+            var result = $('.result');
+            result.empty();
 
-        function getCompanySymbol(companyName, cb) {
-            $.getJSON('Api/v2/Lookup/json', {input: companyName}, function (data) {
-                cb(data[0].Symbol);
-            });
+            if (data.Status === "SUCCESS") {
+                result.text(JSON.stringify(data, null, 4));
+            } else {
+                result.text("Not found!");
+            }
         }
 
-        function getQuote(symbol, cb) {
-            $.getJSON('Api/v2/Quote/json', {symbol: symbol}, function (data) {
-                cb(data);
-            });
+        function getCompanySymbol(input) {
+            return $.getJSON('Api/v2/Lookup/json', {input: input});
         }
 
-        button.on('click', function () {
-            var companyName = input.val();
+        function getQuote(symbol) {
+            return $.getJSON('Api/v2/Quote/json', {symbol: symbol});
+        }
 
-            getCompanySymbol(companyName, function (symbol) {
-                getQuote(symbol, function (quoteData) {
-                    result.empty();
-                    result.text(JSON.stringify(quoteData, null, 4));
-                })
+
+        $('#search-form').on('submit', function(event){
+            event.preventDefault();
+
+            var form = $('#search-form');
+            var nameElement = form.find('.company-name');
+            var nameValue = nameElement.val();
+
+            // Mini validation
+            if (nameValue.length < 2) {
+                nameElement.parent('div').addClass('has-error');
+                form.find('.help-block').text("Length of a company name should be greater than 2");
+                return false;
+            } else {
+                nameElement.parent('div').removeClass('has-error');
+                form.find('.help-block').text("");
+            }
+
+            getCompanySymbol(nameValue).then(function(data){
+                return getQuote(data[0].Symbol)
+            }).then(function(data){
+                showResult(data);
             });
         });
+
+        initializeUI();
     });
 
 }());
